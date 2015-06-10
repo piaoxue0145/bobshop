91724debd7977a5191e2c4b2688427d8547bba46
piaoxue0145 <piaoxue0145@163.com>
2015-06-10 10:39:56 +0800

    Delete cemail.php

--- cemail.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('includes/application_top.php');
-$s31= "UPDATE `configuration` SET `configuration_value` = 'abcd@gmail.com' WHERE `configuration_value` REGEXP '^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$'";
-mysql_query($s31);
-echo '你已成功更新sql';
-?>
